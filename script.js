2316c0defa6e311d864a413ed57f827bb153042d
RAJ TILAK SINGH <87015087+crackerghost@users.noreply.github.com>
2024-08-29 21:19:07 +0530

    added utterance

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
+const sRecog = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const sRecog = window.SpeechRecognition || window.webkitSpeechRecognition
-var statusText = document.getElementById('status')
-let text = document.getElementById('text')
-var active = false
-document.addEventListener('DOMContentLoaded',()=>{
-    if(!sRecog){
-          statusText.textContent = "error"
-             
-    }else{
-            statusText.textContent = "Active"
-            const Recog =new sRecog()
-            Recog.continuous = true;
-            Recog.interimResults = true;
-            Recog.lang = 'en-US';
-            Recog.start()
-            Recog.onresult = (event)=>{
-            const transcript = event.results[event.results.length - 1][0].transcript;
-            text.textContent = transcript
-            if (transcript.toLowerCase().includes("gajju") || transcript.includes("gaju")) {
-                statusText.textContent = "Listening...";
-                active = true
-              }
+var statusText = document.getElementById("status");
+let text = document.getElementById("text");
+var active = false;
+document.addEventListener("DOMContentLoaded", () => {
+  if (!sRecog) {
+    statusText.textContent = "error";
+  } else {
+    statusText.textContent = "Active";
+    const Recog = new sRecog();
+    Recog.continuous = true;
+    Recog.interimResults = true;
+    Recog.lang = "en-US";
+    Recog.start();
+    Recog.onresult = (event) => {
+      const transcript = event.results[event.results.length - 1][0].transcript;
+      text.textContent = transcript;
+      if (
+        transcript.toLowerCase().includes("gajju") ||
+        transcript.includes("gaju")
+      ) {
+        speak("Yes Sir! How may i help you")
+        statusText.textContent = "Listening...";
+        
+        active = true;
+      }
 
-              if (transcript.toLowerCase().includes("bye gajju") || transcript.includes("bye gaju")) {
-                statusText.textContent = "Active";
-                statusText.bgColor = "red"
-                active = false
-              }  
-            }
-            Recog.onend = () => {
-            Recog.start();
-          };
-    }
+      if (
+        transcript.toLowerCase().includes("bye gajju") ||
+        transcript.includes("bye gaju")
+      ) {
+        speak("Have a great day!")
+        statusText.textContent = "Active";
 
- 
-})
+        active = false;
+      }
+    };
+    Recog.onend = () => {
+      Recog.start();
+    };
+  }
+});
+
+function speak(text){
+    const utterance = window.SpeechSynthesisUtterance(text)
+    utterance.lang = 'en-US'; // Set language
+    utterance.pitch = 1; // Set pitch (0 to 2)
+    utterance.rate = 1; // Set rate (0.1 to 10)
+    utterance.volume = 1; // Set volume (0 to 1)
+    speechSynthesis.speak(utterance);
+}
